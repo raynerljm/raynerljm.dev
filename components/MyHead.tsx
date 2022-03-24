8e0ab1aaefe3c4e802b7d8f09512a1fe6eec2bd2
raynerljm <raynerljm@gmail.com>
2022-03-24 11:56:23 +0800

    Add SEO

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -1,19 +1,58 @@
+import { NextSeo } from "next-seo";
 import { FC } from "react";
-import Head from "next/head";
+import { useRouter } from "next/router";
 
 const MyHead: FC = () => {
-  return (
-    <>
-      <Head>
-        <title>Rayner Loh | My Portfolio Website</title>
-        <meta
-          name="Rayner Loh"
-          content="Welcome to my website! I hope you enjoy your stay here and do reach out to me if you would like to chat!"
+    const title = "Rayner Loh";
+    const desc =
+        "Hey! My name is Rayner, and I love building software that makes a positive difference.";
+    const ogImgRelativePath = "/images/untame.jpg";
+
+    const siteURL = "https://raynerljm.me";
+    const ogImageURL = `${siteURL}${ogImgRelativePath}`;
+    const pathName = useRouter().pathname;
+    const pageURL = pathName === "/" ? siteURL : siteURL + pathName;
+    const siteName = "raynerljm.me";
+
+    return (
+        <NextSeo
+            title={title}
+            description={desc}
+            canonical={pageURL}
+            openGraph={{
+                type: "website",
+                locale: "en_US", //  Default is en_US
+                url: pageURL,
+                title,
+                description: desc,
+                images: [
+                    {
+                        url: ogImageURL,
+                        width: 1200,
+                        height: 630,
+                        alt: "raynerljm.me - Rayner's Portfolio",
+                    },
+                ],
+                site_name: siteName,
+            }}
+            additionalMetaTags={[
+                {
+                    property: "author",
+                    content: title,
+                },
+            ]}
+            additionalLinkTags={[
+                {
+                    rel: "icon",
+                    href: `${siteURL}/favicon.ico`,
+                },
+                // {
+                //   rel: "manifest",
+                //   href: "/site.manifest",
+                // },
+            ]}
         />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
-  );
+    );
 };
 
 export default MyHead;
